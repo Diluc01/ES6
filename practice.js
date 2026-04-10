c608f537bf1808f2915ded83ebdd0028ba92a4e8
Berlin Tejeda <berlintejeda22@gmail.com>
2026-04-10 19:39:47 -0400

    solved a new problem

--- practice.js
+++ practice.js
@@ -101,3 +101,16 @@ function isJumping(n) {
 
   return "JUMPING";
 }
+
+function makeAbbr(words) {
+  // write code here
+  let abbreviation = words[0];
+
+  for (let i = 1; i < words.length; i++) {
+    if (words[i] === " ") {
+      abbreviation += words[i + 1];
+    }
+  }
+
+  return abbreviation.toUpperCase();
+}
